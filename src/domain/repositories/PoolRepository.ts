daa9c1f44d32e683392a9e7fb295208a87c0fa6a
vinichagastrexx <vinicius@trexx.com.br>
2023-05-18 16:30:24 -0300

    refactor: addItemToPool return Pool and receive poolId

--- src/domain/repositories/PoolRepository.ts
+++ src/domain/repositories/PoolRepository.ts
@@ -2,7 +2,7 @@ import { Item } from '../models/Item';
 import { Pool } from '../models/Pool';
 
 export interface IPoolRepository {
-  addItemToPool(item: Item): Promise<Item>;
+  addItemToPool(item: Item, poolId: string): Promise<Pool>;
   updatePool(pool: Pool): Promise<boolean>;
   getById(poolId: string): Promise<Pool | null>;
   getAll(): Promise<Pool[]>;
