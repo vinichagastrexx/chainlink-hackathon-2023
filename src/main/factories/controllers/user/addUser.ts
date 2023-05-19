19e6c13cf79f4c85d1869ba39916afd0b60a3cba
viniciushrcs <vinciushrcs@gmail.com>
2023-05-19 18:00:37 -0300

    fix: add user validation name

--- src/main/factories/controllers/user/addUser.ts
+++ src/main/factories/controllers/user/addUser.ts
@@ -1,8 +1,8 @@
 import { makeAddUserUseCase } from '../../usecases/addUser';
 import { Controller } from '../../../../presentation/protocols';
-import { makeUserValidation } from './addUserValidation';
+import { makeAddUserValidation } from './addUserValidation';
 import { AddUserController } from '../../../../presentation/controllers/user/addUserController';
 
 export const makeAddUserController = (): Controller => {
-  return new AddUserController(makeAddUserUseCase(), makeUserValidation());
+  return new AddUserController(makeAddUserUseCase(), makeAddUserValidation());
 };
--- src/main/factories/controllers/user/addUserValidation.ts
+++ src/main/factories/controllers/user/addUserValidation.ts
@@ -1,7 +1,7 @@
 import { Validation } from '../../../../presentation/protocols';
 import { RequiredFieldValidation, ValidationComposite } from '../../../../validation/validators';
 
-export const makeUserValidation = (): ValidationComposite => {
+export const makeAddUserValidation = (): ValidationComposite => {
   const validations: Validation[] = [];
   for (const field of ['walletAddress']) {
     validations.push(new RequiredFieldValidation(field));
